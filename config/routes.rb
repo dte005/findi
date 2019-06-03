925dce8a9a15f867be20163486ee75474e73703c
Daniel Silva <danieldts2013@gmail.com>
2019-06-03 17:18:04 -0300

    Genrerate routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'events#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :user, only: %i[edit update]
+  resources :suggestions, only: %i[edit update show destroy]
+  resources :invites, only: %i[edit update show destroy]
+  resources :events do
+    resources :suggestions, only: %i[new create index]
+    resources :invites, only: %i[new create index]
+    resources :messages, only: %i[new create index]
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_03_193341) do
+ActiveRecord::Schema.define(version: 2019_06_03_195613) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_06_03_193341) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
