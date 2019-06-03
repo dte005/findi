b184f52d4f67362e1430f84dcaddc889b764c11d
Daniel Silva <danieldts2013@gmail.com>
2019-06-03 18:16:35 -0300

    Generate new create actions to controller for event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,15 @@
 class EventsController < ApplicationController
   before_action :find_event, only: %i[show]
+  before_action :event_params, only: %i[create]
 
   def index
     @events = Event.all
+  end
+
+  def new
+  end
+
+  def create
   end
 
   def show
@@ -14,4 +21,7 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
   end
 
+  def event_params
+    params.require(:events).permit(:date, :title, :description)
+  end
 end
