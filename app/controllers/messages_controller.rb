cf958eb567b9f731dc22a91edca86faf16e9895a
Daniel Silva <danieldts2013@gmail.com>
2019-06-04 17:38:23 -0300

    Generate new version new show

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,8 @@
 class MessagesController < ApplicationController
 
   def index
-    @event = Event.find(params[:event_id])
+    @event = Event.find(params[:event_id]) #preciso pegar o evento para restringir o acesso ao usuario errado
     @messages = policy_scope(Message)
-    authorize @event
+    authorize @event #restringindo o acesso
   end
 end
