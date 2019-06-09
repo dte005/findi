4bb6a95954ce311ed58c95e49913b500f6e78159
Gabriel Gil <gabrielbarbosagil@gmail.com>
2019-06-09 16:26:00 -0300

    Fix messagebox width

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,8 @@ module Findi
         end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.time_zone = 'Brasilia'
+    config.active_record.default_timezone = :utc
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
