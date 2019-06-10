a35f2a6641a6820d259639f641a5f46f697bacad
Gabriel Gil <gabrielbarbosagil@gmail.com>
2019-06-10 18:23:59 -0300

    Fix javascript_pack_tag on events index instead of show and add photo uploader to user edit view

--- app/javascript/packs/init_autocomplete.js
+++ app/javascript/packs/init_autocomplete.js
@@ -1,17 +1,6 @@
-// import places from 'places.js';
-
-// const initAutocomplete = () => {
-//   const addressInput = document.getElementById('address-input');
-//   if (addressInput) {
-//     places({ container: addressInput });
-//   }
-// };
-  var places = require('places.js');
-  let placesAutocomplete = places({
-  appId: 'plUAXGNDS1F9',
-  apiKey: 'f625d9f935627225164af3d22587c6a9',
-  container: document.getElementById('suggestion_address')
-    });
-
-
-// export { initAutocomplete };
+var places = require('places.js');
+let placesAutocomplete = places({
+appId: 'plUAXGNDS1F9',
+apiKey: 'f625d9f935627225164af3d22587c6a9',
+container: document.getElementById('suggestion_address')
+  });
