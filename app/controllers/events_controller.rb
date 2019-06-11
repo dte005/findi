8da3f95041a26d2b7e85d29b358022613ba4151d
Gabriel Gil <gabrielbarbosagil@gmail.com>
2019-06-11 17:47:49 -0300

    Add events/invites icons to navbar and improve css

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,7 +6,9 @@ class EventsController < ApplicationController
     @events = policy_scope(Event)
     @pending_invites = Invite.where(email: current_user.email).where(accepted: nil)
     @event = Event.new
-
+    unless @pending_invites.empty?
+      flash[:alert] = ("You have #{@pending_invites.count} invite#{"s" if @pending_invites.count > 1} pending. Click on the " + '<i class="fas fa-envelope"></i>').html_safe
+    end
   end
 
   def create
@@ -20,7 +22,7 @@ class EventsController < ApplicationController
     if @event.save
       redirect_to event_path(@event)
     else
-      # flash[:alert] = @event.errors.values.flatten.first
+      flash[:alert] = @event.errors.values.flatten.first
       render :index
     end
   end
