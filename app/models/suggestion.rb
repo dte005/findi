cc4a5aea29a9d002cd1c99e554b907b9f766472e
Gabriel Gil <gabrielbarbosagil@gmail.com>
2019-06-11 13:49:00 -0300

    Revert "Geocoding"

--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -3,6 +3,4 @@ class Suggestion < ApplicationRecord
   validates :title, presence: true, length: { maximum: 40, too_long: "Suggestion title must have at most 40 characters." }
   validates :address, presence: { message: "Suggestion must have an address." }
 
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
 end
